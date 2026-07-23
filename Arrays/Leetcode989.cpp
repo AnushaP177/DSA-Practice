2d0f46b1487bc958e2d9e03315632f82b27d820d
AnushaP177 <pathakanusha2007@gmail.com>
2026-07-23 21:12:22 +0530

    Add k to vector num and return vector

---
+++ Arrays/Leetcode989.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int i=num.size()-1;
+        while(i>=0 && k){
+            num[i]+=k;
+            k=num[i]/10;
+            num[i]%=10;
+            i--;
+        }
+        
+        while(k){
+             num.insert(num.begin(), k%10);
+            k/=10;
+            }
+        
+        return num;
+    }
+};
